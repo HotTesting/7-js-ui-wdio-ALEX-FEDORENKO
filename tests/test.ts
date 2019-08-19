afa55dfa79ea875f43e239d5e8894586b2e3b2f9
alexander.fedorenko <alexander.fedorenko@wildix.com>
2019-08-19 16:19:04 +0300

    test

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,8 @@
 describe('Protractor', function () {
   it('Shoult be alive', function () {
     browser.url('/0')
-    console.log('--Test passed!')
-  })
+    console.log("Pass")
+  }
+  
+  )
 })
